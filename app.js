73fd8e44445fd612d5bb32ccb09ecd1159406ff4
Henrique Broering Rohden <hrohden@gmail.com>
2023-09-28 10:23:40 -0300

    Adds app.js to serve endpoints

---
+++ app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import { v4 as uuidv4 } from "uuid";
+
+const app = express();
+
+app.get("/moments", (req, res) => {
+  res.send([
+    {
+      id: uuidv4(),
+      title: "(🖥️) Trip to NYC",
+      headline: "My trip to NY with my wife",
+      description:
+        "It was an amazing trip! Many places to take pictures and visit!",
+      isFavorite: false,
+    },
+    {
+      id: uuidv4(),
+      title: "(🖥️) Trip to Europe",
+      headline: "My trip to Europe with my wife in 2014",
+      description:
+        "This trip was when we were waiting for Lucas. Many pictures and great moments.",
+      isFavorite: false,
+    },
+  ]);
+});
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
