f3f7cc13226caf28ad3374a964ba40bc976107ad
Henrique Broering Rohden <hrohden@gmail.com>
2023-09-28 20:08:43 -0300

    Adds nodemon

--- app.js
+++ app.js
@@ -3,25 +3,27 @@ import { v4 as uuidv4 } from "uuid";
 
 const app = express();
 
+const moments = [
+  {
+    id: uuidv4(),
+    title: "(🖥️) Trip to NYC",
+    headline: "My trip to NY with my wife",
+    description:
+      "It was an amazing trip! Many places to take pictures and visit!",
+    isFavorite: false,
+  },
+  {
+    id: uuidv4(),
+    title: "(🖥️) Trip to Europe",
+    headline: "My trip to Europe with my wife in 2014",
+    description:
+      "This trip was when we were waiting for Lucas. Many pictures and great moments.",
+    isFavorite: false,
+  },
+];
+
 app.get("/moments", (req, res) => {
-  res.send([
-    {
-      id: uuidv4(),
-      title: "(🖥️) Trip to NYC",
-      headline: "My trip to NY with my wife",
-      description:
-        "It was an amazing trip! Many places to take pictures and visit!",
-      isFavorite: false,
-    },
-    {
-      id: uuidv4(),
-      title: "(🖥️) Trip to Europe",
-      headline: "My trip to Europe with my wife in 2014",
-      description:
-        "This trip was when we were waiting for Lucas. Many pictures and great moments.",
-      isFavorite: false,
-    },
-  ]);
+  res.send(moments);
 });
 
 app.listen(3000, () => {
