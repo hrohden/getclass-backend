41b95d56b137b6776d13f4ff1abbb387e691b867
Henrique Broering Rohden <hrohden@gmail.com>
2023-10-13 15:01:00 -0300

    Changes current logic to run Prisma

--- src/moments/moments.module.ts
+++ src/moments/moments.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 import { MomentsController } from './moments.controller';
 import { MomentsService } from './moments.service';
 
 @Module({
   controllers: [MomentsController],
-  providers: [MomentsService],
+  providers: [MomentsService, PrismaService],
 })
 export class MomentsModule {}
--- src/moments/moments.service.ts
+++ src/moments/moments.service.ts
@@ -1,64 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 import { v4 as uuidv4 } from 'uuid';
 import { Moment } from './entities/moment.entity';
 
 @Injectable()
 export class MomentsService {
-  moments: Moment[] = [
-    {
-      id: uuidv4(),
-      title: '(🖥️) Trip to NYC',
-      headline: 'My trip to NY with my wife',
-      description:
-        'It was an amazing trip! Many places to take pictures and visit!',
-      isFavorite: false,
-    },
-    {
-      id: uuidv4(),
-      title: '(🖥️) Trip to Europe',
-      headline: 'My trip to Europe with my wife in 2014',
-      description:
-        'This trip was when we were waiting for Lucas. Many pictures and great moments.',
-      isFavorite: false,
-    },
-  ];
+  constructor(private prisma: PrismaService) {}
 
   create(moment: Moment) {
-    const newMoment: Moment = {
+    const data: Moment = {
       id: uuidv4(),
       ...moment,
     };
-    this.moments.push(newMoment);
-    return newMoment;
+    return this.prisma.moment.create({ data });
   }
 
   findAll() {
-    return this.moments;
+    return this.prisma.moment.findMany();
   }
 
   findOne(id: string) {
-    return this.moments.find((moment) => moment.id === id);
+    return this.prisma.moment.findUnique({ where: { id } });
   }
 
-  update(id: string, moment: Moment) {
-    const momentIndex = this.moments.findIndex((moment) => moment.id === id);
-    if (momentIndex < 0) {
-      throw new Error('Moment not found');
-    }
-    this.moments[momentIndex] = {
-      id,
-      ...moment,
-    };
-    return this.moments[momentIndex];
+  update(id: string, data: Moment) {
+    return this.prisma.moment.update({ where: { id }, data });
   }
 
   remove(id: string) {
-    const momentIndex = this.moments.findIndex((moment) => moment.id === id);
-    if (momentIndex < 0) {
-      throw new Error('Moment not found');
-    }
-    const deletedMoment = this.moments[momentIndex];
-    this.moments.splice(momentIndex, 1);
-    return deletedMoment;
+    return this.prisma.moment.delete({ where: { id } });
   }
 }
