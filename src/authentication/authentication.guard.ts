2d163904f6517e45e8b2e11cf70e5702930408a8
Henrique Broering Rohden <hrohden@gmail.com>
2023-10-15 11:18:51 -0300

    Creates Public decorator

--- src/authentication/authentication.guard.ts
+++ src/authentication/authentication.guard.ts
@@ -5,16 +5,26 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
+import { IS_PUBLIC_KEY } from './public.decorator';
 
 @Injectable()
 export class AuthenticationGuard implements CanActivate {
   constructor(
     private jwtService: JwtService,
     private configService: ConfigService,
+    private reflector: Reflector,
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
     if (!token) {
--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
 import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from 'src/users/users.module';
 import { AuthenticationController } from './authentication.controller';
+import { AuthenticationGuard } from './authentication.guard';
 import { AuthenticationService } from './authentication.service';
 
 @Module({
@@ -22,6 +24,12 @@ import { AuthenticationService } from './authentication.service';
     ConfigModule,
   ],
   controllers: [AuthenticationController],
-  providers: [AuthenticationService],
+  providers: [
+    AuthenticationService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthenticationGuard,
+    },
+  ],
 })
 export class AuthenticationModule {}
