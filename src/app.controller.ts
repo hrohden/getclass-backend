d01926b8b73defc43a3ef4f2f65d07d9aca722d3
Henrique Broering Rohden <hrohden@gmail.com>
2023-10-18 17:18:26 -0300

    Removes health checker endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Public } from './authentication/public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get('/health')
-  @Public()
-  getHealthCheck() {
-    return {
-      message: 'Hello World!',
-      version: '1.0.0',
-    };
-  }
 }
