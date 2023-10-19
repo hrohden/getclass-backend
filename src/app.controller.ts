a5c59e66e1d09a095662cf22c320ed8fee6a50b6
Henrique Broering Rohden <hrohden@gmail.com>
2023-10-19 12:49:40 -0300

    Health checker as a public endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './authentication/public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('ping')
+  @Public()
   ping() {
     return {
       message: 'pong',
